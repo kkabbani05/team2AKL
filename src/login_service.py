b2be5bb048a2e181bcc3765f9a5a7c9a222076bc
kkabbani05 <kareem.kabbani@aa.com>
2026-06-26 10:22:01 -0500

    login using cli is done

--- src/login_service.py
+++ src/login_service.py
@@ -2,6 +2,8 @@ import sys
 import board_service
 import api_client 
 import session_manager
+from utils import find_player, read_in_word_list
+import new_game_service
 
 
 def login(player_name: str, registered_players: list):
@@ -42,16 +44,38 @@ def login(player_name: str, registered_players: list):
         # For now, just confirm login succeeded
         #board_service.print_board(board_data)
         pass
+    # if player does not exist
+    if not any(player.name == player_name for player in registered_players):
+        print(f"Could not find user {player_name}. Please register")
+        print(f"Please run 'wurdal register {player_name}' to register")
+        sys.exit(1)
+
+    print(f"Player {player_name} logged in successfully")
+
+    file = open("current_player.txt", "w")
+    file.write(player_name)
+    file.close()
+
+    print(f"May the odds be in your favor {player_name}!")
+
+    player = find_player(player_name, registered_players)[1]
+    if player.game_in_progress:
+        print(f"Here is your current game board, {player_name}:")
+        board_service.print_board(player)
+    elif not player.game_in_progress:
+        print(f"No active game found, starting new game for {player_name}.")
+        word_list = read_in_word_list()
+        new_game_service.new_game(player_name, registered_players, word_list)
 
 
 def logout():
     """
-    Logs out the current player by clearing session and current_player.txt.
+    Logs out the current player by removing the current_player.txt file.
     """
     try:
-        session_manager.clear_session()
-        with open("current_player.txt", "w") as f:
-            f.write("")
+        import os
+
+        os.remove("current_player.txt")
         print("Successfully logged out")
     except FileNotFoundError:
         print("No player is currently logged in.")
