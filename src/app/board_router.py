4ac1845a2364040181e3200b47cb12cc4fd68d08
Angela Leyi Zhang <angela.zhang@aa.com>
2026-07-02 14:18:18 -0500

    Revert "Merge pull request #46 from kkabbani05/api-users-see-correct-letters"
    This reverts commit 72a93a71e2fe4f040124759063f7f65a7a579583, reversing
    changes made to fe82a55cb96a908e51568403b0818b39b18c8af6.

--- src/app/board_router.py
+++ src/app/board_router.py
@@ -10,31 +10,25 @@ from app.session_router import User
 router = APIRouter()
 
 
-class BoardLetterRead(CamelModel):
-    letter: str
-    match: str
-
 class BoardGuessRead(CamelModel):
-    letters: list[BoardLetterRead]
+    attempt_no: int
+    guess_word: str
+    feedback: str
 
-class BoardResultRead(CamelModel):
-    status: str
-    word: str | None
 
 class BoardCurrentRead(CamelModel):
     length: int
     guesses: list[BoardGuessRead]
-    result: BoardResultRead
+
 
 class BoardUserRead(CamelModel):
     id: int
     name: str
 
+
 class BoardRead(CamelModel):
     user: BoardUserRead
     current: BoardCurrentRead
-
-#-------------------------
 
 
 @router.get("/players/{user_id}/board", response_model=BoardRead)
@@ -96,26 +90,17 @@ def get_player_board(
         .all()
     )
 
-    guess_list = []
-    for guess in guesses:
-        letters = [{"letter": guess.guess_word[i].upper(), "match": matches} 
-           for i, matches in enumerate(guess.feedback.split(","))]
-        guess_list.append(letters)
-    
-
     return {
         "user": {"id": user.id, "name": user.name},
         "current": {
             "length": len(current_game.word_to_guess),
             "guesses": [
                 {
-                    "letters": guess
+                    "attempt_no": guess.attempt_no,
+                    "guess_word": guess.guess_word,
+                    "feedback": guess.feedback,
                 }
-                for guess in guess_list
+                for guess in guesses
             ],
-            "result": {
-                "status": current_game.status,
-                "word": None
-            }
         },
     }
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,5 @@
 import sys
-import os 
 import argparse
-from pathlib import Path
 import requests
 from pydantic import TypeAdapter
 from models import Player, Word, Guess, Record
@@ -24,8 +22,7 @@ def read_in_word_list():
     return: words: list of words for the player to guess.
     """
     try:
-        word_list_path = Path(__file__).with_name("word_list.txt")
-        with word_list_path.open("r") as f:
+        with open("../src/word_list.txt", "r") as f:
             words = f.read().split("\n")
         return words
     except FileNotFoundError:
@@ -199,24 +196,6 @@ def write_players(registered_players: list):
     """
     Writes players to persisted storage
     """
-    ##NEW 
-
-    expected_keys = {
-        "name",
-        "current_word_index",
-        "current_word",
-        "game_in_progress",
-        "seen_words",
-        "record",
-    }
-
-    if not registered_players:
-        return
-
-    if not all(isinstance(player, dict) and expected_keys.issubset(player.keys()) for player in registered_players):
-        return
-
-    ## NEW
     try:
         adapter = TypeAdapter(list[Player])
         json_bytes = adapter.dump_json(registered_players, indent=4)
